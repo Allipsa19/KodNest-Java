11060a51259c1d48e2392919169e265b129b4520
Allipsa19 <allipsavananijally@gmail.com>
2026-09-04 14:17:06 +0530

    I am in Java Track

---
+++ track/Main.java
@@ -0,0 +1,10 @@
+package track;
+
+public class Main {
+    public static void main(String[] args) {
+        // Display the student's learning track.
+        System.out.println("track: Java Development");
+        // Display the student's daily commitment.
+        System.out.println("Daily Practice: 8 hours");
+    }
+}
