bf13c066705ddbc5e0433f098e9490cc11a8e552
Allipsa19 <allipsavananijally@gmail.com>
2026-09-16 19:39:31 +0530

    feat: implement task CRUD operations

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Message.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Message.java
@@ -1,5 +1,5 @@
-public class Message{
-    public static void main(String[] args){
+public class Message {
+    public static void main(String[] args) {
         System.out.println("Hello KodNest");
     }
 }
