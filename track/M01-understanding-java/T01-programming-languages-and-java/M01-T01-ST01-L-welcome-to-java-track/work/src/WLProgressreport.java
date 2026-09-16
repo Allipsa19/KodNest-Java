0c89e3ba8482f295849de9e89bac4a8053e85baf
Allipsa19 <allipsavananijally@gmail.com>
2026-09-16 20:25:05 +0530

    I am in my java track

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WLProgressreport.java
@@ -0,0 +1,8 @@
+public class WLProgressreport {
+    public static void main(String[] args) {
+        System.out.println("Completed Topics: 17");
+        System.out.println("Remaining Topics: 3");
+        System.out.println("Weekly Learning Hours: 15");
+        System.out.println("Progress Percentage: 85.0");
+    }
+}
