68d7c2ea3fe74e51a58de2c539335df56d784891
Allipsa19 <allipsavananijally@gmail.com>
2026-09-19 12:40:56 +0530

    I'm in my java track

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Printnumf1ton.java
@@ -0,0 +1,9 @@
+public class Printnumf1ton {
+    public static void main(String[] args) {
+        int limit = 5;
+
+        for (int i = 1; i <= limit; i++ ){
+            System.out.println(i);
+        }
+    }
+}
