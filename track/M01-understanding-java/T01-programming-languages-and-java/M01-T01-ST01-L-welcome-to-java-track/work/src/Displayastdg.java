412e98590a98dea993d28a37d0c005fdcc4e6775
Allipsa19 <allipsavananijally@gmail.com>
2026-09-19 12:30:07 +0530

    I'm in my java track

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/Displayastdg.java
@@ -0,0 +1,15 @@
+public class Displayastdg {
+    public static void main(String[] args) {
+        int marks = 76;
+
+        if (marks >= 80) {
+            System.out.println("Grade: A");
+        } else if (marks <= 79) {
+            System.out.println("Grade: B");
+        } else if (marks <= 59){
+            System.out.println("Grade: C");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+}
